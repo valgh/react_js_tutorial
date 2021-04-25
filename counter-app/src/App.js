71551280c11ecd5d11a5175d59052995b8e233c3
Valerio <trenta.valerio@gmail.com>
2021-04-25 15:26:53 +0200

    finished

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import Navbar from './components/navbar'
 import './App.css';
+import Counters from './components/counters';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+ 
+  constructor(props) {
+    super(props);
+    this.state = { 
+      counters: [
+        { id: 1, value: 3, selected: true },
+        { id: 2, value: 1, selected: true },
+        { id: 3, value: 0, selected: true }
+      ]
+    };
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  componentDidMount() {
+    // Component was mounted on DOM.
+    // AJAX CALLS are usually performed here.
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // called right after an update has been detected in the DOM and a part of the component has been 
+    // updated with new values.
+    console.log(prevProps);
+    console.log(prevState);
+    // we can decide here whether we should make an AJAX call to get new data in here!
+    // if (prevProps.counter[0].value !== this.state.counter[0].value) {
+      // make AJAX CALL
+    //}
+  }
+
+  componentWillUnmount() {
+    // Component is unmounted from DOM.
+  }
+
+  handleDelete = (idToDelete) => {
+      this.setState({ counters: this.state.counters.filter(c => c.id !== idToDelete )})
+  }
+
+  handleReset = () => {
+      const counters = this.state.counters.map(counter => {
+          counter.value = 0;
+          return counter;
+      });
+      this.setState({ counters: counters });
+  }
+
+  handleIncrement = (counter) => {
+      const counters = [...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = { ...counter };
+      counters[index].value++;
+      this.setState({ counters: counters });
+  }
+
+  render() {
+    // Component is rendered here.
+    return (
+      <React.Fragment>
+        <Navbar counters={this.state.counters.filter(counter => counter.value > 0).length}/>
+        <main className="container">
+          <Counters 
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,56 +4,42 @@ export default class Counter extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            count: 0,
-            tags: ['tag1', 'tag2', 'tag3']
-        };
         this.style = { 
-            fontSize: 35,
+            fontSize: 20,
             fontWeight: 'bold'
         };
-
-        this.handleIncrement = this.handleIncrement.bind(this);
     }
 
     formatCount() {
-        const { count } = this.state;
         const zeroFormat = <span>Zero</span>
-        return count === 0 ? zeroFormat : count;
+        return this.props.counter.value === 0 ? zeroFormat : this.props.counter.value;
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
-    renderList() {
-        if (this.state.tags.length === 0) {
-            return <p>There are no Tags!</p>
-        } else {
-            return this.state.tags.map(tag => <li key={tag}>{tag}</li>);
-        }
-    }
-
-    handleIncrement() {
-        this.setState({ count: this.state.count+1 });
-    }
 
     render() { 
         return (  
             <React.Fragment>
+                <h4>ITEM #{this.props.counter.id}</h4>
                 <span style={this.style} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 {' '}
                 <button 
-                    onClick={() => this.handleIncrement()} 
+                    onClick={() => this.props.onIncrement(this.props.counter)} 
                     className="btn btn-secondary btn-sm"
                 >
                     Increment
                 </button>
-                <ul>
-                    {this.renderList()}
-                </ul>
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm m-2"
+                >
+                    Delete
+                </button>
             </React.Fragment>
         );
     }
